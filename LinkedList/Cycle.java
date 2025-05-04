7a4c3fa9eff96134d6362f1e10a6d02e912421de
akshatp1811 <akshatpanday28@gmail.com>
2025-05-05 02:09:15 +0530

    Test Commit

---
+++ LinkedList/Cycle.java
@@ -0,0 +1,19 @@
+package LinkedList;
+
+
+public class Cycle {
+
+    class ListNode {
+             int val;
+             ListNode next;
+             ListNode(int x) {
+                 val = x;
+                 next = null;
+             }
+         }
+         public boolean hasCycle(ListNode head) {
+            ListNode fast = head;
+            ListNode slow = head;
+        }
+        return false;
+     }

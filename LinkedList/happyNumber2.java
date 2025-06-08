8f592550654d4f60099f775b5cfd918b77278ebf
akshatp1811 <akshatpanday28@gmail.com>
2025-06-08 22:52:23 +0530

    Create happyNumber2.java

---
+++ LinkedList/happyNumber2.java
@@ -0,0 +1,34 @@
+public class happyNumber2 {
+    public class ListNode {
+        int val;
+        ListNode next;
+    }
+    public int calcSum(int num)
+    {
+        int sum = 0;
+        while (num>0) {
+            sum = sum + (int)Math.pow(num%10,2);
+            num = num/10;
+        }
+       return sum; 
+    }
+    public boolean isHappy(int n) {
+    int sum = 0;
+    int tempnum = num;
+    fast = head;
+    slow = head;
+
+    
+        while (sum !=  1)
+    {
+        sum = calcSum(tempnum);
+        ListNode node = new ListNode();
+        node.val = sum;
+        node.next = node;//
+        //insert the new node in the end of the node 
+        }
+    }
+
+
+    
+}

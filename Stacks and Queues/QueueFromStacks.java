d0730acb0a766a2fd1784aeb92c40fcc05ef2607
akshatp1811 <akshatpanday28@gmail.com>
2025-06-13 03:29:49 +0530

    QueueAsStack

--- Stacks and Queues/QueueFromStacks.java
+++ Stacks and Queues/QueueFromStacks.java
@@ -8,8 +8,9 @@ public class QueueFromStacks {
             System.out.println("Stack OverFLow");
             return;
         }
-        tos++;
-        stack[tos] = val;
+        if (stack == stack1) tos1++;
+        else if (stack == stack2) tos2++;
+        stack[getTos(stack)] = val;
     }
 
     static int pop(int[] stack)
@@ -18,105 +19,161 @@ public class QueueFromStacks {
             System.out.println("Stack UnderFlow");
             return -1;
         }
-        int value =stack[tos];
-        tos--;
+        int value = stack[getTos(stack)];
+        if (stack == stack1) tos1--;
+        else if (stack == stack2) tos2--;
         return value;
-        
     }
+
     static int peek(int[] stack) {
         if (isEmpty(stack)) {
             System.out.println("Stack is empty");
             return -1;
         }
-        return stack[tos];
+        return stack[getTos(stack)];
     }
 
     static boolean isFull(int[] stack)
     {
-        if (tos == stack.length - 1) {
-            return true;
-        }
-        else return false;
+        if (stack == stack1) return tos1 == stack.length - 1;
+        else if (stack == stack2) return tos2 == stack.length - 1;
+        return false;
     }
+
     static boolean isEmpty(int[] stack)
     {
-        if (tos == -1) {
-            return true;
-        }
-        else return false;
+        if (stack == stack1) return tos1 == -1;
+        else if (stack == stack2) return tos2 == -1;
+        return true;
     }
+
     static void printStack(int[] stack) {
         if (isEmpty(stack)) {
             System.out.println("Stack is empty");
             return;
         }
         System.out.print("Stack elements: ");
-        for (int i = 0; i <= tos; i++) {
+        for (int i = 0; i <= getTos(stack); i++) {
             System.out.print(stack[i] + " ");
         }
         System.out.println();
     }
 
-//+++++++++++++++++++++++++++++++Implementing Queue from stacks++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // +++++++++++++++++++++++++++++++ Implementing Queue from stacks ++++++++++++++++++++++++++++++++++++++++++++++++++++
     static int[] stack1 = new int[5];
     static int[] stack2 = new int[5];
+    static int tos1 = -1;
+    static int tos2 = -1;
 
     static void QueuePush(int val){
         push(val, stack1);
-        int valu = pop(stack1);
-        push(valu,stack2);
     }
+
     static int QueuePop(){
-        return pop(stack2);
+         if (isEmpty(stack1)) {
+        System.out.println("Queue is empty");
+        return -1;
+    }
+        while (!isEmpty(stack1)) {
+            push(pop(stack1), stack2);
+        }
+        int valu = pop(stack2);
+        while (!isEmpty(stack2)) {
+            push(pop(stack2), stack1);
+        }
+        return valu; 
     }
 
-
+    static int getTos(int[] stack) {
+        if (stack == stack1) return tos1;
+        else if (stack == stack2) return tos2;
+        return tos; // fallback (used for original stack[])
+    }
 
     public static void main(String[] args) {
-        // pushQueue(5 , stack);
-        // printStack(stack);
-        // push(4 , stack);
-        // printStack(stack);
-        // push(3 , stack);
-        // printStack(stack);
-        // push(2 , stack);
-        // printStack(stack);
-        // push(1 , stack);
-        // printStack(stack);
-        // System.out.println(pop(stack));
-        // printStack(stack);
-        // System.out.println(pop(stack));
-        // printStack(stack);
-        // System.out.println(pop(stack));
-        // printStack(stack);
-        // System.out.println(pop(stack));
-        // printStack(stack);
-        // System.out.println(pop(stack));
-        // printStack(stack);
-
         QueuePush(5);
-        printStack(stack);
+        printStack(stack1);
         QueuePush(4);
-        printStack(stack);
+        printStack(stack1);
         QueuePush(3);
-        printStack(stack);
+        printStack(stack1);
         QueuePush(2);
-        printStack(stack);
+        printStack(stack1);
         QueuePush(1);
-        printStack(stack);
+        printStack(stack1);
         System.out.println(QueuePop());
-        printStack(stack);
+        printStack(stack1);
         System.out.println(QueuePop());
-        printStack(stack);
+        printStack(stack1);
         System.out.println(QueuePop());
-        printStack(stack);
+        printStack(stack1);
         System.out.println(QueuePop());
-        printStack(stack);
+        printStack(stack1);
         System.out.println(QueuePop());
-        printStack(stack);
-        
-
-
-        
+        printStack(stack1);
     }
 }
+
+
+/* 
+Leetcode Solutions
+class MyQueue {
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+
+    public MyQueue() {
+    }
+
+    public void push(int x) {
+        stack1.push(x); 
+    }
+    
+     public int pop() {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+
+        int val = stack2.pop(); // front of queue
+
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop()); // restore
+        }
+
+        return val;
+    }
+    
+    public int peek() {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+         int val = stack2.peek(); 
+
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop()); 
+        }
+
+        return val;
+    }
+    
+    public boolean empty() {
+        return stack1.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+*/

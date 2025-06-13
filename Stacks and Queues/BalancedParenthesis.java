677fcd4529b3056673994debe5fb560ed3e14d9a
akshatp1811 <akshatpanday28@gmail.com>
2025-06-13 23:41:21 +0530

    Edit in Code

--- Stacks and Queues/BalancedParenthesis.java
+++ Stacks and Queues/BalancedParenthesis.java
@@ -1,4 +1,89 @@
+import java.util.*;
+
 public class BalancedParenthesis {
-    
-    
+    String str = "({[})]";
+    Stack<Character> stack = new Stack<>();
+    public boolean isBalanced = true;
+
+    boolean isbalancedParenthesis(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(' || str.charAt(i) == '{' || str.charAt(i) == '[') {
+                stack.push(str.charAt(i)); // fixed: changed str.charAt[i] to str.charAt(i)
+            } else {
+                if (stack.isEmpty()) return false; // prevent popping from empty stack
+                char paren = stack.pop();
+                char expectedOpen;
+                switch (str.charAt(i)) {
+                    case ')':
+                        expectedOpen = '(';
+                        break;
+                    case '}':
+                        expectedOpen = '{';
+                        break;
+                    case ']':
+                        expectedOpen = '[';
+                        break;
+                    default:
+                        expectedOpen = '\0';
+                }
+                if (paren == expectedOpen) { // fixed: str.charAt[i] -> paren
+                    isBalanced = true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return isBalanced; // fixed: variable name was isbalanced
+    }
+
+    public static void main(String[] args) {
+        BalancedParenthesis bp = new BalancedParenthesis();
+        System.out.println(bp.isbalancedParenthesis(bp.str));
+    }
 }
+
+
+
+// import java.util.*;
+// public class BalancedParenthesis {
+//     String str = "()[{}(])";
+//     Stack<Character> stack = new Stack<>();
+//     public boolean isBalanced = true;
+//     boolean isbalancedParenthesis(String str){
+//     for(int i = 0;i < str.length() ; i++){
+//          if(str.charAt(i) == '(' || str.charAt(i) == '{' || str.charAt(i) == '[')
+//         {
+//             stack.push(str.charAt[i]);
+//         }
+//         else{
+//             char paren = stack.pop();
+//             char expectedOpen;
+//             switch (str.charAt(i)) {
+//                 case ')':
+//                     expectedOpen = '(';
+//                     break;
+//                 case '}':
+//                     expectedOpen = '{';
+//                     break;
+//                 case ']':
+//                     expectedOpen = '[';
+//                     break;
+//                 default:
+//                     expectedOpen = '\0';
+//             }
+//             if (str.charAt[i] == expectedOpen) {
+//                 isbalanced = true;
+//             }
+//             else{
+//                 return false;
+//             };
+//         }
+
+//     }
+//     return isbalanced;
+// }
+// }
+
+
+    
+

ce68de163965ff3d1b2a3915195a63a8a5c492a0
Akshat Pandey <akshatpanday28@gmail.com>
2026-05-14 13:45:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (31.77%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -3,7 +3,8 @@ class Solution {
         int n = s.length();
         int count = 0;//To store the number of brackets opened till now when this becomes 0 then depth becomes 0;
         int depth = 0;
-        int[] depthArr = new int[n];
+        //int[] depthArr = new int[n];
+        int maxDepth = 0;
         for(int i = 0; i < n; i++){
             if(s.charAt(i) == '('){
                 count++;
@@ -16,10 +17,10 @@ class Solution {
                     depth = 0;
                 }
             }
-            depthArr[i] = depth;
+            maxDepth = Math.max(maxDepth, depth);
             
         }
-        int max = Arrays.stream(depthArr).max().getAsInt();
-        return max;
+        //int max = Arrays.stream(depthArr).max().getAsInt();
+        return maxDepth;
     }
 }
